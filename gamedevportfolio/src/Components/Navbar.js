60a294793683b475e12b61f55df9406d0f68c6d2
balramThakur <99987850+balramThakur@users.noreply.github.com>
2024-02-24 09:25:47 +0000

    .

---
+++ gamedevportfolio/src/Components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='navbar'>
+        <h3>Portfolio 😄</h3>
+        <ul>
+            <li>
+                Home
+            </li>
+            <li>
+                About
+            </li>
+            <li>
+                Contact Me
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
