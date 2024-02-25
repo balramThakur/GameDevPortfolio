a9324e9ed2430a81b0e4ab65f0880ef641b81b8b
balramThakur <99987850+balramThakur@users.noreply.github.com>
2024-02-25 09:20:35 +0000

    .

--- gamedevportfolio/src/Pages/Home.js
+++ gamedevportfolio/src/Pages/Home.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import Navbar from '../Components/Navbar'
-
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import profilePicture from '../Images/profile-picture.jpg';
 function Home() {
   return (
     <div className='home'>
         <Navbar/>
+        <div className='profile'>
+          <img src={profilePicture} alt='Profile'/>
+        </div>
     </div>
   )
 }
