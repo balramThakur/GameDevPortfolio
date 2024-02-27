60a6808fe5d00248c7942d4df751b378d2b729bb
balramThakur <99987850+balramThakur@users.noreply.github.com>
2024-02-27 07:28:44 +0000

    .

--- gamedevportfolio/src/Pages/Home.js
+++ gamedevportfolio/src/Pages/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
 import profilePicture from '../Images/profile-picture.jpg';
+import gmail from '../Images/gmail.png';
+import phoneCall from '../Images/phone-call.png';
+import linkedin from '../Images/linkedin.png';
 import Fullpage, { FullPageSections, FullpageSection } from '@ap.cx/react-fullpage';
 import ZombieKillerGame from '../Video/ZombieKillerGame.mp4';
 import { FaChevronDown } from "react-icons/fa";
@@ -18,10 +21,24 @@ function Home() {
             <div className='home'>
               <Navbar />
               <div className='profile'>
-                <img src={profilePicture} alt='Profile' />
-                <h2>
-                  Hi, My name is Balram Thakur And I am a Unity 2d and 3d Game Developer
-                </h2>
+                <img src={profilePicture} alt='Profile' className='profileImg' />
+                <div>
+                  <h2>
+                    Hi, My name is Balram Thakur And I am a Unity 2d and 3d Game Developer
+                  </h2>
+                  <div className='gmailContainer'>
+                    <img src={gmail} alt='Gmail' />
+                    <a href="https://mail.google.com/mail/u/1/?view=cm&fs=1&to=balramthakur159@gmail.com&tf=1
+">balramthakur159@gmail.com</a>
+                  </div>
+                  <div className='phoneNumber'>
+                    <img src={phoneCall} alt='Gmail' />
+                    <a href="tel:932-664-7491">9326647491</a>
+                  </div>
+                  <div className='linkedin'>
+                    <img src={linkedin} alt='linkedin'/>
+                  </div>
+                </div>
               </div>
               <div className='myProjectsHeading'>
                 <h2>
@@ -32,21 +49,45 @@ function Home() {
             </div>
           </FullpageSection>
           <div className='videoPlayer'>
-            {
-              GameVideo.map((videos) => <FullpageSection style={{ with: "100vh", padding: "14vh 0" }}>
-                <div className='videoContainer'>
-                  <video width="800" autoPlay="true" loop="true" controls >
-                    <source src={videos} type="video/mp4" />
-                  </video>
-                  <div>
-                    <p>
-                      "Undead Road Rampage: Drive, Destroy, Survive! Engage in heart-pounding action as you navigate through an endless road infested with zombies. Run over the undead, earn points, and upgrade your car's health or unlock a rocket launcher for ultimate destruction. Can you survive the apocalypse? Play now and find out!"
-                    </p>
-                  </div>
+            <FullpageSection style={{ with: "100vh", padding: "14vh 0" }}>
+              <div className='videoContainer'>
+                <video width="800" autoPlay="true" loop="true" controls >
+                  <source src={GameVideo[0]} type="video/mp4" />
+                </video>
+                <div>
+                  <p>
+                    {GameTitle[0]}
+                  </p>
                 </div>
-              </FullpageSection>
-              )
-            }
+              </div>
+            </FullpageSection>
+            <FullpageSection style={{ with: "100vh", padding: "14vh 0" }}>
+              <div className='videoContainer'>
+                <video width="800" autoPlay="true" loop="true" controls >
+                  <source src={GameVideo[1]} type="video/mp4" />
+                </video>
+                <div>
+                  <p>
+                    {GameTitle[1]}
+                  </p>
+                </div>
+              </div>
+            </FullpageSection>
+            <FullpageSection style={{ with: "100vh", padding: "14vh 0" }}>
+              <div className='videoContainer'>
+                <video width="800" autoPlay="true" loop="true" controls >
+                  <source src={GameVideo[2]} type="video/mp4" />
+                </video>
+                <div>
+                  <h3>
+                    {GameTitle[2]}
+                  </h3>
+                  <p>
+
+                  </p>
+                </div>
+              </div>
+            </FullpageSection>
           </div>
         </FullPageSections>
       </Fullpage>
